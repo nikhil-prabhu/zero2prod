8f5a6c5f2e8dcd2e8cc8396082ba0a194b83d52b
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2022-06-08 10:57:17 +0000

    Reorganize definitions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,16 @@
 use std::net::TcpListener;
 
+/// Spins up an instance of our application
+/// and returns its address (i.e. http://localhost:XXXX)
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind on random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     spawn_app();
@@ -14,13 +25,4 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind on random port.");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{}", port)
 }
